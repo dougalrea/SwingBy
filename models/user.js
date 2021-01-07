b2f2ba21f47ea062e4fb2cd343a99c2eee881652
Ash Smith <ashleynathansmith@gmail.com>
2021-01-07 21:34:12 +0000

    deleted changes

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true, maxlength: 20 },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
   }
 )
 
