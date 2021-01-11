268da030864837ef170239de7588bbde143711ca
Ash Smith <ashleynathansmith@gmail.com>
2021-01-11 20:40:26 +0000

    updated person show ui

--- client/src/components/people/ProfileShow.js
+++ client/src/components/people/ProfileShow.js
@@ -2,7 +2,8 @@
 import React from 'react'
 import { getOnePerson } from '../../lib/api'
 import { useParams } from 'react-router-dom'
-import { Box, Heading, Text, Image, Flex, Spacer, Button, Stack, Badge } from '@chakra-ui/react'
+import { Box, Heading, Text, Image, Flex, Spacer, Button, Stack, Badge, FormControl,
+  FormLabel, FormErrorMessage, FormHelperText, Input, Avatar, AvatarBadge, Wrap, WrapItem, Divider } from '@chakra-ui/react'
 
 function ProfileShow() {
   const [person, setPerson] = React.useState(null)
@@ -41,13 +42,14 @@ function ProfileShow() {
             </Flex>
             <Flex>
               <Box
+                flexGrow={2}
                 w='350px'
                 rounded='20px'
                 boxShadow='sm'
                 key={person._id}>
-                <Image p={5} align='left'rounded='20px'src={person.profilePicture} />
+                <Image p={5} align='left'rounded='40px'src={person.profilePicture} />
               </Box>
-              <Box direction='column'm={5} p={5}>
+              <Box flexGrow={1} direction='column'm={5} p={5}>
                 <Text>Age: {person.age}</Text>
                 <Text>Gender: {person.gender}</Text>
                 <Text>Sexual Orientation: {person.sexualOrientation}</Text>
@@ -57,9 +59,34 @@ function ProfileShow() {
                 <Text>Open to drugs: {person.isOpenToDrugs}</Text>
                 <Text>Food Preferences: {person.foodPreferences}</Text>
               </Box>
-              <Box>
+              <Box flexGrow={1}>
                 <Text>Reviews go here</Text>
               </Box>
+              <box flexGrow={1}>
+                <Wrap border='2px' m={5}>
+                  <WrapItem>
+                    <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+                  </WrapItem>
+                  <WrapItem>
+                    <Avatar name="Kola Tioluwani" src="https://bit.ly/tioluwani-kolawole" />
+                  </WrapItem>
+                  <WrapItem>
+                    <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+                  </WrapItem>
+                  <WrapItem>
+                    <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
+                  </WrapItem>
+                  <WrapItem>
+                    <Avatar name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
+                  </WrapItem>
+                  <WrapItem>
+                    <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
+                  </WrapItem>
+                  <WrapItem>
+                    <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+                  </WrapItem>
+                </Wrap>
+              </box>
             </Flex>
             <Stack isInline align='baseline'>
               <Box ml={5} justify='space-around'>
@@ -71,10 +98,39 @@ function ProfileShow() {
                 <Badge m={1} variant='solid' variantColor='white' rounded='full' px={2}>{person.interests[3]}</Badge>
               </Box>
             </Stack>
+            <Divider p={2}/>
             <Flex bg='.200'>
               <Text p={5}>Hello gents – thanks for stopping by. I’m a creative, fun-loving, energetic and active girl whose favourite words – in any language – are ‘Please proceed to your gate for departure’. I’ve been bitten HARD by the travel bug and have been lucky enough to visit every continent (well, I’m working on Antarctica). I love going out, meeting new people and generally getting the most out of life – whether that’s trying new bars and restaurants or picking up a new sport (I once joined in a game of pick-up basketball with a group of kids in Zimbabwe – it was great!) You should shoot me a message if you’re fun-loving, fit, and up for anything – I am.</Text>
             </Flex>
-
+            <Box w='500px' m={3} p={3}>
+              <FormControl>
+                <FormLabel>Contact {person.firstName}</FormLabel>
+                <Input bg='blue.200' type='email' />
+                <FormHelperText>Say hello or ask a question</FormHelperText>
+              </FormControl>
+            </Box>
+            <Flex
+              justify='space-between'>
+              <Box
+                w='350px'
+                rounded='20px'
+                boxShadow='sm'>
+                <Image p={5} align='left'rounded='40px'src={person.profilePicture} />
+              </Box>
+              <Box
+                w='350px'
+                rounded='20px'
+                boxShadow='sm'>
+                <Image p={5} align='left'rounded='40px'src={person.profilePicture} />
+              </Box>
+              <Box
+                w='350px'
+                rounded='20px'
+                boxShadow='sm'>
+                <Image p={5} align='left'rounded='40px'src={person.profilePicture} />
+              </Box>
+            </Flex>
+            <Divider orientation="horizontal" />
           </Flex>
         </Box>
         :
