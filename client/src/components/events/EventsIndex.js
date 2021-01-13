efe2e46c8a44afe9e1c418076bd26f604fe4881b
isaaclf <isaac.lundie-fallon@hotmail.co.uk>
2021-01-13 20:37:57 +0000

    event index map

--- client/src/components/events/EventsIndex.js
+++ client/src/components/events/EventsIndex.js
@@ -1,14 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { getAllEvents } from '../../lib/api'
-import { ChakraProvider, Heading, Wrap, Box, Tabs, TabList, TabPanels, Tab, TabPanel, Spinner } from '@chakra-ui/react'
+import { ChakraProvider, Heading, Wrap, Box, Tabs, TabList, TabPanels, Tab, TabPanel, Spinner, Badge } from '@chakra-ui/react'
 import EventCard from '../../components/events/EventCard'
-import EventMapIndexCard from '../../components/events/EventMapIndexCard'
 import Fonts from '../../styles/Fonts'
 import { extendTheme } from '@chakra-ui/react'
-
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import { Link } from 'react-router-dom'
 
 function EventsIndex() {
   const [events, setEvents] = React.useState(null)
+  const [viewport, setViewport] = React.useState({
+    latitude: 51.515,
+    longitude: -0.078,
+    zoom: 9
+  })
+  const [popupData, setPopupData] = React.useState(null)
 
   React.useEffect(() => {
     const getData = async () => {
@@ -22,6 +29,8 @@ function EventsIndex() {
     getData()
   }, [])
 
+
+
   const theme = extendTheme({
     fonts: {
       heading: 'Dancing Script',
@@ -32,7 +41,14 @@ function EventsIndex() {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
-      <Heading align='center' m={5} as='h1' fontSize='96px' color='pink.800'>SwingBy</Heading>
+      <Heading
+        align='center'
+        m={5} as='h1'
+        fontSize='96px'
+        color='pink.800'
+      >
+        SwingBy
+      </Heading>
       {events ? 
         <Box m={50}>
           <Tabs>
@@ -48,8 +64,70 @@ function EventsIndex() {
                   ))}
                 </Wrap>
               </TabPanel>
-              <TabPanel>
-                <EventMapIndexCard />
+              <TabPanel
+                maxW='100%'
+                w='85vw' 
+                h='65vh'
+                mt={2}
+                borderWidth='1px' 
+                borderRadius='lg'
+                borderColor='gray.500'
+              >
+                <ReactMapGL
+                  mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+                  height="100%"
+                  width="100%"
+                  mapStyle='mapbox://styles/mapbox/outdoors-v11'
+                  latitude={viewport.latitude}
+                  longitude={viewport.longitude}
+                  zoom={viewport.zoom}
+                  onViewportChange={viewport => setViewport(viewport)}
+                >
+                  {events &&
+                    events.map(event => (
+                      <Marker
+                        key={event._id}
+                        latitude={Number(event.latitude)}
+                        longitude={Number(event.longitude)}
+                      >
+                        <span onClick={() => setPopupData(event)}>
+                          🎯
+                        </span>
+                      </Marker>
+                    ))
+                  }
+                  {popupData &&
+                    <Popup
+                      latitude={Number(popupData.latitude)}
+                      longitude={Number(popupData.longitude)}
+                      onClose={() => setPopupData(null)}
+                    >
+                      <div style={{ width: '100px' }}>
+                        <img src={popupData.imageURL} style={{ width: '100%', height: 'auto' }}/>
+                      </div>
+                      <p className="event-name">
+                        <Link to={`/events/${popupData._id}`}>
+                          {popupData.name.toUpperCase()}
+                        </Link>
+                      </p>
+                      <p className="event-types">{popupData.types.join(' ')}</p>
+                      <Badge variant="outline" colorScheme='purple' rounded='full' fontSize='0.6em' mr={2}> 
+                        {new Date(popupData.startDateTime).toLocaleDateString()}
+                      </Badge>
+                      <Badge variant="outline" colorScheme='green' rounded='full' fontSize='0.6em'>
+                        {new Date(popupData.startDateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                      </Badge>
+                      <p className="event-spaces">
+                        {popupData.capacity - popupData.attendees.length}/{popupData.capacity} spaces remaining
+                      </p>
+                      <p className="event-host">
+                        hosted by <Link to={`/people/${popupData.owner._id}`}>
+                          {popupData.owner.firstName}
+                        </Link>
+                      </p>
+                    </Popup>
+                  }
+                </ReactMapGL>
               </TabPanel>
             </TabPanels>
           </Tabs>
